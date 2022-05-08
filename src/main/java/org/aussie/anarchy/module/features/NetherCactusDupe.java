cb5fb870702f535ad293f7ba42a3b6b2617931bb
RemainingToast <Remainingtoast@gmail.com>
2022-05-09 03:50:41 +1000

    fix dupe

--- src/main/java/org/aussie/anarchy/module/features/NetherCactusDupe.java
+++ src/main/java/org/aussie/anarchy/module/features/NetherCactusDupe.java
@@ -4,9 +4,9 @@ import org.aussie.anarchy.module.Module;
 import org.aussie.anarchy.util.config.Config;
 import org.bukkit.World;
 import org.bukkit.entity.ChestedHorse;
+import org.bukkit.entity.Donkey;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.entity.EntityPortalEvent;
-import org.bukkit.inventory.ItemStack;
 
 public class NetherCactusDupe extends Module {
 
@@ -20,31 +20,47 @@ public class NetherCactusDupe extends Module {
         return this;
     }
 
+    private int cloneId = -1;
+
     @EventHandler
     private void on(EntityPortalEvent e) {
-        if (e.getEntity() instanceof ChestedHorse) {
-            ChestedHorse inventory = (ChestedHorse) e.getEntity();
+        if (e.getEntity() instanceof Donkey) {
+            Donkey inventory = (Donkey) e.getEntity();
 
             if (!inventory.isCarryingChest()) return;
 
             if (!(e.getFrom().getWorld().getEnvironment() == World.Environment.NORMAL)) return;
 
             // Check Donkey is on Half a Heart
-            if (inventory.getHealth() <= 1) {
+            if (inventory.getHealth() <= 1 && inventory.getEntityId() != this.cloneId) {
                 World world = inventory.getWorld();
+                world.spawn(inventory.getLocation(), inventory.getClass(), clone -> {
+                    this.cloneId = clone.getEntityId();
+                    this.copyInventory(inventory, clone);
 
-                world.spawnEntity(inventory.getLocation(), inventory.getType(), inventory.getEntitySpawnReason(), entity -> {
-                    ChestedHorse clone = (ChestedHorse) entity;
-                    ItemStack[] items = inventory.getInventory().getContents();
-
-                    clone.setHealth(inventory.getHealth());
-                    clone.getInventory().setContents(items);
+                    clone.getInventory().setSaddle(inventory.getInventory().getSaddle());
+                    clone.setMaxDomestication(inventory.getMaxDomestication());
+                    clone.setDomestication(inventory.getDomestication());
+                    clone.setJumpStrength(inventory.getJumpStrength());
                     clone.setCustomName(inventory.getCustomName());
-                    clone.setCarryingChest(inventory.isCarryingChest());
+                    clone.setHealth(inventory.getHealth());
                     clone.setOwner(inventory.getOwner());
+                    clone.setAdult();
                 });
             }
         }
     }
+
+    private void copyInventory(ChestedHorse from, ChestedHorse to) {
+        if (from instanceof Donkey && to instanceof Donkey) {
+            Donkey d1 = (Donkey) from;
+            Donkey d2 = (Donkey) to;
+            d2.setCarryingChest(d1.isCarryingChest());
+            d2.getInventory().setContents(d1.getInventory().getContents());
+        } else {
+            /* Do nothing */
+        }
+    }
+
 
 }
